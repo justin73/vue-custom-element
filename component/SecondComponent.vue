fba0cdd410ea75189238040766ae8e093a1808e8
Meng Jia <jiameng_73@hotmail.com>
2018-01-28 13:13:05 -0500

    added second vue component

---
+++ component/SecondComponent.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SecondComponent',
+  data () {
+    return {
+      msg: 'This is the second vue component wrapped by vue-custom-element'
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #668bdb;
+}
+</style>
--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import DemoElement from './component/HiThere.vue';
+import DemoElement2 from './component/SecondComponent.vue';
 import 'document-register-element/build/document-register-element'
 import Vue from 'vue'
 // include vue-custom-element plugin to Vue
 import VueCustomElement from 'vue-custom-element'
 Vue.use(VueCustomElement)
 Vue.customElement('demo-basic', DemoElement);
-
+Vue.customElement('second-component', DemoElement2);
 class VueElement {
 }
 
