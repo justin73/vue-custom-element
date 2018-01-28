dfb54468ed6e71fc68097d9ddd8cab4c6229b8a3
Meng Jia <jiameng_73@hotmail.com>
2018-01-28 00:48:50 -0500

    update vue element usage

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import DemoElement from './component/HiThere.vue';
 import 'document-register-element/build/document-register-element'
 import Vue from 'vue'
+// include vue-custom-element plugin to Vue
+import VueCustomElement from 'vue-custom-element'
+Vue.use(VueCustomElement)
 Vue.customElement('demo-basic', DemoElement);
 
 class VueElement {
